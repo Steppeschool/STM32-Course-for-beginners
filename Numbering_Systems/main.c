1c66e52fdebfb15f26fa097aa51692d5beaeb1ae
Steppeschool <contact@steppeschool.com>
2022-10-04 17:17:05 +0200

    Pushing numbering systems

---
+++ Numbering_Systems/main.c
@@ -0,0 +1,12 @@
+
+#include <stdio.h>
+
+int main()
+{
+	int x = 345, y = 0b001001000111101011101110 ; // 101 = 5;
+	int z = 0x247aee; // 001001000111101011101110
+	printf("x is %x \n", x);
+	printf("y is %x \n", y);
+	printf("z is %x \n", z);
+	return 0;
+}
